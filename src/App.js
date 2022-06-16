886da828b15ecc294d4dda0278c8e2e310dbb35f
jgbg30 <josegregoriobarretogonzalez@gmail.com>
2022-06-15 22:37:44 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import logo from '../src/img/logo-circular-texto.png';
+import Navbar from './componentes/Navbar';
+import Cards from './componentes/Cards';
+import Footer from './componentes/Footer';
+import Centro from './componentes/Centro';
 
 function App() {
   return (
-    <div className="container-fluid">
-      <header className="container-fluid d-flex flex-column align-items-center mt-5">
-      <h1 className="text-center">Bienvenido al template simple de React</h1>
-         <div className="w-25">
-        <img src={logo} className="w-100" alt="logo" />
-         </div>
-        <p>
-          Edita <code>src/App.js</code> y salva el cambio para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendé React
-        </a>
-      </header>
+    <div className="container">
+      
+      <Navbar />
+      <Centro/>
+      
+      <Cards />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/componentes/Centro.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import carrousel3 from '../verga/carrousel-3.jpg'
+
+
+const Centro = () => {
+  return (
+
+    
+    <div>
+       <img src={carrousel3} width="100%" alt="carrousel3" />
+      
+    </div>
+  )
+}
+
+export default Centro
